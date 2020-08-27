08403fcf0c8a388dadef1fe49c1823f033236e6f
franklin,petra <petrafranklin.gmail.com>
2020-08-27 16:03:21 -0700

    Updates

---
+++ precache-manifest.d760c730bc1d565f6ad61d4225f2be9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea530b93bf9bfc244646c69457098fe",
+    "url": "/cook_this/index.html"
+  },
+  {
+    "revision": "b734cfba649f588ae8db",
+    "url": "/cook_this/static/css/main.fa7a3558.chunk.css"
+  },
+  {
+    "revision": "14d486d6994f445c91f8",
+    "url": "/cook_this/static/js/2.44864974.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cook_this/static/js/2.44864974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b734cfba649f588ae8db",
+    "url": "/cook_this/static/js/main.56a058a2.chunk.js"
+  },
+  {
+    "revision": "6eb1060c4e7c91bc84ac",
+    "url": "/cook_this/static/js/runtime-main.392f2087.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cook_this/precache-manifest.d760c730bc1d565f6ad61d4225f2be9c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cook_this/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
